776d0cba54d88fa5af5a1c59ef1a97379ed62ef2
Eduardo Aliaga <aaaacccc_aaaacccc@hotmail.com>
2016-11-28 10:58:43 -0500

    trap

--- graph.cpp
+++ graph.cpp
@@ -8,7 +8,7 @@ class Graph
     public:
         Graph(const vector<int> & , const vector<int> & );
         int numOutgoing(const int ) const;
-        const vector<int> adjacent(const int nodeID) const;
+        const vector<int>& adjacent(int nodeID) const;
 
     private:
     	vector<int> out, to;
@@ -20,24 +20,24 @@ Graph::Graph(const vector<int> &stars, const vector<int> &ends)
 	int a, b;
 	this->out=stars;
 	this->to=ends;
-	
+
 	cout << "coloque numero de nodos: " << endl;
 	cin>> b;
-	this->nodes=b; 
+	this->nodes=b;
 	for (int i=0; i<b; i++){
 		cout << "coloque node de salida: " << endl;
 		cin >> a;
-		stars.push_back(a);
+		out.push_back(a);
 		cout << "ahora su nodo de entrada: " << endl;
 		cin >> a;
-		ends.push_back(a);
+		to.push_back(a);
 	}
 };
 
 int Graph::numOutgoing(const int nodeID) const
 {
 	int a=0;
-	
+
 	for (int i=0; i<nodes; i++){
 		if (out[i]==nodeID)
 			a=a+1;
@@ -45,9 +45,9 @@ int Graph::numOutgoing(const int nodeID) const
 	return a;
 };
 
-const vector<int> &Graph::adjacent(const int nodeID) const
+const vector<int>& Graph::adjacent(int nodeID) const
 {
-	vector<int> a;
+	static vector<int> a;
 	for (int i=0; i<nodes; i++){
 		if (out[i]==nodeID)
 			a.push_back(to[i]);
@@ -57,6 +57,6 @@ const vector<int> &Graph::adjacent(const int nodeID) const
 
 int main()
 {
-	Graph raf(entrada, salida);
+	Graph raf(vector<int> entrada, vector<int> salida);
 	return 0;
 }
